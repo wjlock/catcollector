a2666bb8b0ddca6679a67b1c954d2b88097b2ea8
Will <will.locklier@gmail.com>
2021-01-14 12:46:38 -0500

    Add model and cats to db

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,23 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def cats_index(request):
+    cats = Cat.objects.All()
+    return render(request, 'cats')
+
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
