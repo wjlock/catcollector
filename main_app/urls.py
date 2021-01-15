28d6f822da0288cdda4226b2ecd403ea92b2ff63
Will <will.locklier@gmail.com>
2021-01-15 10:27:07 -0500

    Add cat toy model, more delete/create routes for cat toys?

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('cats/', views.cats_index, name="cats_index"),
+    path('cats/<int:cat_id>', views.cats_show, name="cats_show"),
+    path('cats/create/', views.CatCreate.as_view(), name="cats_create"),
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name="cats_update"),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name="cats_delete"),
+    path('user/<username>/', views.profile, name="profile"),
+    path('cattoys/', views.cattoys_index, name='cattoys_index'),
+    path('cattoys/<int:cattoy_id>', views.cattoys_show, name='cattoys_show'),
+    path('cattoys/create/', views.CatToyCreate.as_view(), name='cattoys_create'),
+    path('cattoys/<int:pk>/update/',
+         views.CatToyUpdate.as_view(), name='cattoys_update'),
+    path('cattoys/<int:pk>/delete/',
+         views.CatToyDelete.as_view(), name='cattoys_delete')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Cat, CatToy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -10,22 +14,77 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
+########### CATS ################
+
 
 def cats_index(request):
-    cats = Cat.objects.All()
-    return render(request, 'cats')
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
 
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', {'cat': cat})
 
 
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect('/cats')
+
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/cats')
+
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats'
+
+########## USER ##############
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    cats = Cat.objects.filter(user=user)
+    return render(request, 'profile.html', {'username': username, 'cats': cats})
+
+
+### CatToy ####
+def cattoys_index(request):
+    cattoys = CatToy.objects.all()
+    return render(request, "cattoys/index.html", {'cattoys': cattoys})
+
+
+def cattoys_show(request, cattoy_id):
+    cattoy = CatToy.objects.get(id=cattoy_id)
+    return render(request, 'cattoys/show.html', {'cattoy': cattoy})
+
+
+class CatToyCreate(CreateView):
+    model = CatToy
+    fields = '__all__'
+    success_url = '/cattoys'
+
+
+class CatToyUpdate(UpdateView):
+    model = CatToy
+    fields = ['name', 'color']
+    success_url = '/cattoys'
+
+
+class CatToyDelete(DeleteView):
+    model = CatToy
+    success_url = '/cattoys'
